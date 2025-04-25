fcb375c88c29a5c1067c08dfe07ee2ea2a841efb
Vidhan Gupta <electronicavid@gmail.com>
2025-04-25 08:08:50 +0530

    add Object Oriented Concepts

--- ObjectOrientedProgramming/student.py
+++ ObjectOrientedProgramming/student.py
@@ -1,27 +1,42 @@
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing Name")
-        if house not in ["Gryffindor", "Huffelpuf", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
-    
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
 
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Huffelpuf", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing Name")
+        self._name = name
+
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+
+
 def main():
-    student = get_student()
-    print(student)
-
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    student = Student(name, house)
-    return student
+    student = Student.get()
+    print(f"{student.name} from {student.house}")
 
 
 if __name__ == "__main__":
